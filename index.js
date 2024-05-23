a0e983efe767a421b737dc4ee85dc8bcb2528a88
tana2013 <ammari2019pro@gmail.com>
2024-05-23 22:06:08 +0300

    include test folder

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res)=>{
- res.send("Home Route ^^")
+ res.send({"response":"Home Route ^^"})
 })
-app.get('/login', (req, res)=>{
-    res.send("Login Route ^^")
+app.get('/will', (req, res)=>{
+    res.send({"response":" Hello World"})
+})
+app.get('/ready', (req, res)=>{
+    res.send({"response":"Great it Works *^-^*"})
 })
 app.get('/admin', (req, res)=>{
-    res.send("Admin Dashboard Route ^^")
+    res.send({"response":"Admin Dashboard Route ^^"})
 })
 
 app.listen(process.env.PORT || 3000);
